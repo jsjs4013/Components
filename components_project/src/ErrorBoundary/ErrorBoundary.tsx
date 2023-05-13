702e4954824c370df5025ea077acdc8648e1fcda
Joowon Moon <jsjs401@daum.net>
2023-05-14 00:39:55 +0900

    feat Eslint, Prettier 적용

--- components_project/src/ErrorBoundary/ErrorBoundary.tsx
+++ components_project/src/ErrorBoundary/ErrorBoundary.tsx
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 
 interface State {
-    hasError: boolean;
-    error: Error | null;
+  hasError: boolean;
+  error: Error | null;
 }
 
-class ErrorBoundary extends Component<React.PropsWithChildren<{}>, State> {
+class ErrorBoundary extends Component<React.PropsWithChildren<object>, State> {
   state = { hasError: false, error: null };
 
   // 오류를 발생시킨 후 UI 업데이트
@@ -15,7 +15,7 @@ class ErrorBoundary extends Component<React.PropsWithChildren<{}>, State> {
 
   // 오류 기록 & 리포팅
   componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {
-    console.error("Caught error:", error, errorInfo);
+    console.error('Caught error:', error, errorInfo);
   }
 
   render() {
@@ -23,7 +23,7 @@ class ErrorBoundary extends Component<React.PropsWithChildren<{}>, State> {
       return <h1>Something went wrong!</h1>;
     }
 
-    return this.props.children; 
+    return this.props.children;
   }
 }
 
--- components_project/src/modal/modal.tsx
+++ components_project/src/modal/modal.tsx
@@ -2,26 +2,26 @@ import { useEffect, useRef, useState, ReactElement } from 'react';
 import ReactDOM from 'react-dom';
 
 interface ModalProps {
-    children: ReactElement;
+  children: ReactElement;
 }
 
 const Modal = ({ children }: ModalProps) => {
-    const ref = useRef<HTMLElement | null>(null);
-    const [mounted, setMounted] = useState<boolean>(false);
+  const ref = useRef<HTMLElement | null>(null);
+  const [mounted, setMounted] = useState<boolean>(false);
 
-    useEffect(() => {
-        const dom = document.getElementById('modal');
-        if (dom) {
-            ref.current = dom;
-            setMounted(true);
-        }
-    }, []);
-
-    if (ref.current && mounted) {
-        return ReactDOM.createPortal(children, ref.current);
+  useEffect(() => {
+    const dom = document.getElementById('modal');
+    if (dom) {
+      ref.current = dom;
+      setMounted(true);
     }
+  }, []);
 
-    return null;
-}
+  if (ref.current && mounted) {
+    return ReactDOM.createPortal(children, ref.current);
+  }
+
+  return null;
+};
 
 export default Modal;
